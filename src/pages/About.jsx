35db622bdcee44c629c25299b6ac567042dbb48e
Hannah <108206521+ayemyatlwin@users.noreply.github.com>
2024-01-25 21:49:53 +0630

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,47 @@
 import React from "react";
 import Header from "../components/Header";
-import Button from "../components/Button";
+
+// function User(name) {
+//   this.name = name;
+//   this.isAdmin = false;
+// }
+
+// let userOne = new User("Jack");
+// let userTwo = new User("John");
+// let userThree = new User("Joe");
+
+// console.log(userOne);
+// console.log(userThree);
+// console.log(userTwo);
+
+function User(name) {
+  this.name = name;
+
+  this.sayHi = function() {
+    alert( "My name is: " + this.name );
+  };
+}
+
+let hannah = new User("Hannah");
+
+
+/*
+john = {
+   name: "John",
+   sayHi: function() { ... }
+}
+*/
+
 
 const About = () => {
+  
+
   return (
     <>
       <Header />
-      About
-      <Button btnText="Click me"/>
+      <h1>About Page</h1>
+      <button onClick={()=>hannah.sayHi()}>Hi</button>
+      
     </>
   );
 };
